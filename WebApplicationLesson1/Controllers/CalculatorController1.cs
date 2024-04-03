31149d0b557bbe0a2f070e4321bf9417f132de73
zalina.dzboeva@outlook.com <zalina.dzboeva@outlook.com>
2024-04-03 23:52:15 +0300

    создала контролер калькулятор

--- WebApplicationLesson1/Controllers/CalculatorController1.cs
+++ WebApplicationLesson1/Controllers/CalculatorController1.cs
@@ -3,15 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
-    public class CalculatorController1 : Controller
+    public class CalculatorController : Controller
     {
-        public class CalculatorController : Controller
+        public string Index(int a, int b)
         {
-            public string Index(int a, int b)
-            {
-                return $"{a} + {b} = {a + b}";
-            }
+            return $"{a} + {b} = {a + b}";
         }
-
     }
 }
