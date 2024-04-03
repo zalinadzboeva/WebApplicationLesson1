14b054091d00722b5752706fed223ab1603317b7
zalina.dzboeva@outlook.com <zalina.dzboeva@outlook.com>
2024-04-03 23:18:36 +0300

    добавила контроллер start и метод hello которые выводят время в моменте по определенному правилу

---
+++ WebApplicationLesson1/Controllers/StartController1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class StartController1 : Controller
+    {
+        public string hello()
+        {
+            var time = DateTime.Now.Hour;
+            if (time < 6)
+            {
+                return "Доброй ночи";
+            }
+            if (time < 12)
+            {
+                return "Доброе утро";
+            }
+            if (time < 18)
+            {
+                return "Добрый день";
+            }
+            return "Добрый вечер";
+        }
+    }
+}
